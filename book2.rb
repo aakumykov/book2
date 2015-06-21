d23d1b7fae33da7cd31c6d724c5070125ea50101
Andrey <andrey@debian-test2>
2015-06-21 23:02:35 +0700

    Игнорирование *.log

--- book2.rb
+++ book2.rb
@@ -579,7 +579,7 @@ end
 
 book = Book.new('test book',{
 	:depth=>1,
-	:pages=>5,
+	:pages=>3,
 	:threads=>1,
 	:db_type => 'memory'
 })
@@ -588,11 +588,8 @@ book = Book.new('test book',{
 #book.addSource('https://ru.wikipedia.org/wiki/%D0%9E%D1%80%D1%83%D0%B6%D0%B5%D0%B9%D0%BD%D1%8B%D0%B9_%D0%BF%D0%BB%D1%83%D1%82%D0%BE%D0%BD%D0%B8%D0%B9')
 #book.addSource('https://ru.wikipedia.org/wiki/Оружейный_плутоний')
 #book.addSource('https://ru.wikipedia.org/wiki/Амёба')
-book.addSource('https://ru.wikipedia.org/wiki/Союз_Советских_Социалистических_Республик')
-
-#book.addSource('https://ru.wikipedia.org/wiki/Большевики')
-#book.addSource('https://ru.wikipedia.org/wiki/Советский_народ')
-#book.addSource('https://ru.wikipedia.org/wiki/СССР')
+#book.addSource('https://ru.wikipedia.org/wiki/Союз_Советских_Социалистических_Республик')
+book.addSource('https://ru.wikipedia.org/wiki/СССР')
 #book.addSource('https://ru.wikipedia.org/wiki/Гражданская_война_в_России')
 
 
