b9f0e1ef7c341620c396a7ba86d3a582e62762ff
Андрей Кумыков <aakumykov@yandex.ru>
2015-07-07 16:49:55 +0700

    Ковыряю simple-tidy

---
+++ tidy.rb
@@ -0,0 +1,114 @@
+#coding: utf-8
+
+require 'open3'
+
+class SimpleTidy
+
+  DEFAULT_OPTIONS = { :tidy_mark => false }
+
+  OPTIONS_WITHOUT_VALUES = [ :indent, :utf8, :ashtml, :asxhtml, :asxml, :numeric ]
+
+  SINGLE_DASH_OPTIONS = OPTIONS_WITHOUT_VALUES
+
+  def initialize options = nil
+    @tidy_options = DEFAULT_OPTIONS.merge(options || {})
+  end
+
+  def warnings
+    @warnings || []
+  end
+
+  def clean html
+     @output, @warnings = get_output_and_warnings html
+     @output
+  end
+
+  def self.clean html, options = nil
+    SimpleTidy.new(options).clean html
+  end
+
+private
+
+  def get_output_and_warnings html
+    stdin, stdout, stderr = Open3.popen3 "tidy #{ tidy_options_text } #{ temp_file(html) }"
+    output   = stdout.read.strip
+    warnings = stderr.read.split("\n").select {|line| line =~ /line \d+ column \d+ - Warning:/ }
+    return output, warnings
+  end
+
+  def tidy_options
+
+    # check options for dependencies (some options require other options to be set)
+    if @tidy_options.keys.include? :indent_spaces and ! @tidy_options[:indent] == true
+      @tidy_options[:indent] = true
+    end
+
+    @tidy_options
+  end
+
+  def tidy_options_text
+    tidy_options.map {|key, value| 
+      option = dashes_for_option key
+      option << key.to_s.gsub('_', '-')
+      option << " #{ value.to_s.gsub('_', '-') }" unless OPTIONS_WITHOUT_VALUES.include?(key)
+      option
+    }.join(' ')
+  end
+
+  def dashes_for_option option
+    SINGLE_DASH_OPTIONS.include?(option) ? '-' : '--'
+  end
+
+  def temp_file html
+    filename = "/tmp/simple-tidy-#{ Time.now.strftime('%Y%m%d%H%M%S') }.html"
+    File.open(filename, 'w'){|f| f << html }
+    filename
+  end
+
+end
+
+
+class SimpleTidyMy < SimpleTidy
+	DEFAULT_OPTIONS = { :tidy_mark => false }
+
+	OPTIONS_WITHOUT_VALUES = [ :indent, :utf8, :ashtml, :asxhtml, :asxml, :numeric ]
+
+	SINGLE_DASH_OPTIONS = OPTIONS_WITHOUT_VALUES
+end
+
+
+
+st = SimpleTidy.new({
+	
+	:force_output => 'yes',
+	:drop_proprietary_tags => 'yes',
+	:doctype => 'omit',
+	
+	:utf8 => true,
+	:asxml => true,
+	:numeric => true,
+	
+})
+
+
+case ARGV.count
+when 1
+	input_file = ARGV[0]
+	output_file = 'out.xhtml'
+when 2
+	input_file = ARGV[0]
+	output_file = ARGV[1]
+else
+	puts "Usage #{__FILE__} <input.html>"
+	exit 1
+end
+
+
+File.open(output_file,'w') { |file|
+       file.write( 
+		  st.clean(File.read(input_file))
+	 )
+}
+
+
+system "ls -lh #{output_file}"
