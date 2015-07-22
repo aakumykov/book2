7e058a7b5d6a5f0d771604c7ae1750b02c45c556
Андрей Кумыков <aakumykov@yandex.ru>
2015-07-22 16:47:39 +0700

    loadPage() с перекодировкой встроена в код.

--- book2.rb
+++ book2.rb
@@ -4,7 +4,7 @@
 system 'clear'
 
 require 'rubygems'
-require 'open-uri'
+require 'open3'
 require 'sqlite3'
 require 'securerandom'
 require 'digest/md5'
@@ -12,7 +12,7 @@ require 'tmpdir'
 require 'tempfile'
 require 'fileutils'
 require 'nokogiri'
-require 'curl'	# must be before 'colorize'
+require 'net/http'
 require 'colorize'	# must be after 'curl' for right colors
 require 'awesome_print'
 require 'uri'
@@ -422,13 +422,78 @@ QWERTY
 		return res
 	end
 	
-	def loadPage(uri)
-		msg_info_blue "#{__method__}('#{uri.urlencoded? ? URI::decode(uri) : uri}')"
-		
-		curl = CURL.new
-		curl.user_agent = @options[:user_agent]
-		page = curl.get(uri)
+	def loadPage ( uri, redirects_limit=10)
 
+		def detectCharset (arg)
+
+			page = arg[:page]
+			headers = arg[:headers].nil? ? {} : arg[:headers]
+
+			page_charset = nil
+			headers_charset = nil
+
+			pattern = Regexp.new(/charset\s*=\s*['"]?(?<charset>[^'"]+)['"]?/i)
+
+			res = page.encode('UTF-8',{:replace => '_',:invalid => :replace,:undef => :replace}).match(pattern)
+			page_charset = res[:charset].upcase if not res.nil?
+
+			headers.each_pair { |k,v|
+				if 'content-type'==k.downcase.strip then
+					res = v.first.downcase.strip.match(pattern)
+					headers_charset = res[:charset].upcase if not res.nil?
+				end
+			}
+
+			return page_charset if headers_charset.nil?
+			return headers_charset
+			
+			#~ return {
+				#~ :headers_charset => headers_charset,
+				#~ :page_charset => page_charset,
+			#~ }
+		end
+
+
+		raise ArgumentError, 'слишком много перенаправлений' if redirects_limit == 0
+
+
+		begin 
+			uri = URI(uri) 
+		rescue 
+			uri = URI( URI.escape(uri) ) 
+		end
+
+
+		data = {}
+
+		Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+
+			request = Net::HTTP::Get.new uri.request_uri
+			request['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0'
+			
+			response = http.request request
+
+			case response
+			when Net::HTTPRedirection then
+				location = response['location']
+				puts "перенаправление на '#{location}'"
+				warn "redirected to #{location}"
+				data =  loadPage(location, redirects_limit - 1)
+			when Net::HTTPSuccess then
+				data = {
+					:headers => response.to_hash,
+					:page => response.body,
+				}
+			else
+				response.value
+			end
+		end
+		
+		
+		charset = detectCharset(data)
+		
+		page = data[:page].encode('UTF-8', charset, { :replace => '_', :invalid => :replace, :undef => :replace })
+		
 		return page
 	end
 	
@@ -962,11 +1027,12 @@ book = Book.new(
 		:language => 'ru',
 	},
 	:source => [
-		'https://ru.wikipedia.org/wiki/Самооценка',
+		#'https://ru.wikipedia.org/wiki/Самооценка',
+		'http://opennet.ru'
 	],
 	:options => {
-		:depth => 5,
-		:total_pages => 3,
+		:depth => 1,
+		:total_pages => 5,
 		:pages_per_level =>2,
 		
 		:threads => 1,
