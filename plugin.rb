f28061f97a55919206c31e902fc0fb2f412897d8
Андрей Кумыков <aakumykov@yandex.ru>
2015-09-01 16:51:55 +0700

    Что-то пробую, пока в раздумьях.

--- plugin.rb
+++ plugin.rb
@@ -11,51 +11,56 @@ class Master
 	}
 
 	def initialize
-		#puts "#{self}.#{self.class}.#{__method__}"
 		@@a = 10
 		@@b = 20
 	end
 
 	def self.plugin(name,data)
-		p = Object.const_get(name).new
+		plugin = Object.const_get(name).new
+		puts "@: #{plugin.to_s}"
+		
 		@@options[:plugin_log] << name
-		p.work(data)
-	end
-
-	def plugin(name,data)
-		self.class.plugin(name,data)
-	end
-
-	def self.get_option(key)
-		@@options[key]
-	end
-
-	def set_option(key,value)
-		@@options[key] = value
+		
+		plugin.set_uuid 'some UUID'
+		
+		plugin.work(data)
 	end
 end
 
 class PluginSkel
+	attr_reader :uuid
+
 	def initialize
-		#puts "#{self}.#{self.class}.#{__method__}"
+		puts "initialize #{self.class}"
+	end
+	
+	def set_uuid(value)
+		@uuid = value
+	end
+	
+	def work(data=nil)
+		puts "#{self.class}.#{__method__}(), #{self}, #{caller}"
 	end
 end
 
 class Plugin1 < PluginSkel
 	def work(data)
-		new_data = "#{data} -*- #{self.class}.#{__method__} -*- #{Master.get_option(:name)}"
-		Master.plugin('Plugin2',new_data)
+		super
+		new_data = "#{data} / #{self.class}.#{__method__}"
+		new_data = Master.plugin('Plugin2',new_data)
 	end
 end
 
 class Plugin2 < PluginSkel
 	def work(data)
-		"#{data} -*- #{self.class}.#{__method__} -*- #{Master.get_option(:depth)}"
+		super
+		new_data = "#{data} / #{self.class}.#{__method__}"
 	end
 end
 
 class Html < PluginSkel
 	def work(data)
+		super
 		new_data = Master.plugin('Clear',data)
 		new_data = Master.plugin('Txt',new_data)
 	end
@@ -63,19 +68,21 @@ end
 
 class Txt < PluginSkel
 	def work(data)
-		new_data = "#{data} -*- #{self.class}.#{__method__} -*- #{Master.get_option(:depth)}"
+		super
+		new_data = "#{data} / #{self.class}.#{__method__}"
 		Master.plugin('Clear',new_data)
 	end
 end
 
 class Clear < PluginSkel
 	def work(data)
-		"#{data} -*- #{self.class}.#{__method__} -*- #{Master.get_option(:depth)}"
+		super
+		new_data = "#{data} / #{self.class}.#{__method__}"
 	end
 end
 
 
 m = Master.new
 puts Master.plugin('Plugin1','initial data')
-puts Master.plugin('Html','initial data')
-puts "call log: #{Master.get_option(:plugin_log).join(',')}"
+#puts Master.plugin('Html','initial data')
+#puts "call log: #{Master.get_option(:plugin_log).join(',')}"
