9ad2b7009aab9dc0dd6164e228258c2eaae5f1f1
Andrey Kumykov <aakumykov@yandex.ru>
2015-09-21 00:08:08 +0700

    Показывает работу фильтр ru.wikipedia.org

--- plugins/www/filters/ru_wikipedia_org_filter.rb
+++ plugins/www/filters/ru_wikipedia_org_filter.rb
@@ -9,6 +9,11 @@ class RuWikipediaOrgFilter < FilterSkel
 
 	def article(arg)
 		Msg.blue "#{self.class}.#{__method__}(#{arg})"
+		Book.plugin(
+			name: 'www/load',
+			data: arg[:uri],
+			uuid: SecureRandom.uuid,
+		)
 	end
 
 	def article_printable(arg)
